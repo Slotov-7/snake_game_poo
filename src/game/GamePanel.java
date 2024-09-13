3d792e9b53bbc3911aa95305833aedf9c7f0e4d3
lexlemos <allexlemos12@gmail.com>
2024-09-13 11:29:26 -0300

    game over

--- src/game/GamePanel.java
+++ src/game/GamePanel.java
@@ -47,10 +47,11 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void draw(Graphics g) {
+       if(running){
         for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
             g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
             g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
-        }
+            }
         g.setColor(randomColor());
         g.fillOval(foodX, foodY, UNIT_SIZE, UNIT_SIZE);
 
@@ -58,11 +59,15 @@ public class GamePanel extends JPanel implements ActionListener {
             if(i==0){
                 g.setColor(Color.green);
                 g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-            }
+                }
             else{
                 g.setColor(new Color(45,180,0));
                 g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                }
             }
+        }
+        else{
+            gameOver(g);
         }
     }
 
@@ -136,7 +141,11 @@ public class GamePanel extends JPanel implements ActionListener {
         }
     }
     public void gameOver(Graphics g) {
-
+        //texto do Game Over
+        g.setColor(Color.red);
+        g.setFont(new Font("Ink Free", Font.BOLD, 75));
+        FontMetrics metrics = getFontMetrics(g.getFont());
+        g.drawString("Game Over", (SCREEN_WIDTH - metrics.stringWidth("Game Over"))/2, SCREEN_HEIGHT/2);
     }
 
     @Override

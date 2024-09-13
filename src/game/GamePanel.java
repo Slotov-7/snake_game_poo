c2de5aac686b61b59798a246eac48cfb5da0d1ac
Slotov-7 <guilhermguilhermegui888@gmail.com>
2024-09-13 11:30:35 -0300

    corrigindo a cor da comida

--- src/game/GamePanel.java
+++ src/game/GamePanel.java
@@ -22,6 +22,7 @@ public class GamePanel extends JPanel implements ActionListener {
     public int foodsEaten;
     public int foodX;
     public int foodY;
+    private  Color foodColor;
     public char direction = 'D';
     public boolean running = false;
     Timer timer;
@@ -36,6 +37,7 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void startGame() {
+        foodColor = randomColor();
         generateFood();
         running = true;
         timer = new Timer(DELAY, this);
@@ -51,7 +53,7 @@ public class GamePanel extends JPanel implements ActionListener {
             g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
             g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
         }
-        g.setColor(randomColor());
+        g.setColor(foodColor);
         g.fillOval(foodX, foodY, UNIT_SIZE, UNIT_SIZE);
 
         for(int i = 0; i < bodyParts;i++){
@@ -76,6 +78,7 @@ public class GamePanel extends JPanel implements ActionListener {
     public void generateFood() {
         foodX = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
         foodY = random.nextInt((int) (SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
+        foodColor = randomColor();
     }
     public void move() {
         for (int i = bodyParts; i > 0 ; i--) {
